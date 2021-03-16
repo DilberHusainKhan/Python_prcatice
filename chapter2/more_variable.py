4085ed9a09566679ff75588b3857557288f691c4
Dilber Husain Khan <53649013+DilberHusainKhan@users.noreply.github.com>
2021-03-16 20:15:36 +0530

    chapter 2 about variable

---
+++ chapter2/more_variable.py
@@ -0,0 +1,10 @@
+name, age = "Dilber", 24
+print("hello "+ name+' you age is '+ str(age))
+
+# number_one , number_two = input("enter two number seprated by").split(",") 
+# print( f'The sum of two number is {int(number_one)+int(number_two)}')
+
+one , two = input("Enter two number").split(" ")
+total= int(one)+int(two)
+print('The Sum of two number{} and {} is {}'.format(one,two, total))
+
