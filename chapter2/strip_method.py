e98dc5971dd3fe60595e58d5f06fba87d1f13aac
Dilber Husain Khan <53649013+DilberHusainKhan@users.noreply.github.com>
2021-03-18 12:57:25 +0530

    strip method to remove spaces

---
+++ chapter2/strip_method.py
@@ -0,0 +1,11 @@
+name= "         Huma  yun          "
+dot= "..........."
+print(name+dot)
+# lstrip
+print(name.lstrip()+dot)
+# rstrip
+print(name.rstrip()+dot)
+# strip
+print(name.strip()+dot)
+# replace
+print(name.replace(" ", ""))
