f62b1a61435228ebe5f3f152c883b702a1bb02fd
Dilber Husain Khan <53649013+DilberHusainKhan@users.noreply.github.com>
2021-03-18 12:59:37 +0530

    string method

---
+++ chapter2/string_method_1.py
@@ -0,0 +1,11 @@
+# length function
+Name = "HumaYUN hhhhhh"
+print(len(Name))
+# lower method
+print(Name.lower())
+# upper method
+print(Name.upper())
+# Title method
+print(Name.title())
+# count method
+print(Name.count("h"))
