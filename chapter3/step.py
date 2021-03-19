72af803694b396abf5f0f971acee840a6f81aa28
Dilber Husain Khan <53649013+DilberHusainKhan@users.noreply.github.com>
2021-03-19 12:46:33 +0530

    update step file

--- chapter3/step.py
+++ chapter3/step.py
@@ -1,7 +1,7 @@
 for i in range(1,11,2):
     print(i)
-# for i in range(0,11,2):
-#     print(i)
+for i in range(0,11,2):
+    print(i)
 # for i in range(2,11,2):
 #     print(i)
 # for i in range(12,1,-2):
