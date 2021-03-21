ac011d4d1af65284e0e918072d09e147599e0cfd
Dilber Husain Khan <53649013+DilberHusainKhan@users.noreply.github.com>
2021-03-21 15:24:17 +0530

    final tuple

---
+++ chapter6/final_tuple.py
@@ -0,0 +1,9 @@
+# Something more about tuple 
+num = tuple(range(1,11))
+print(num)
+nums = str((1,2,3,4,5,6,7,8,9,10))
+print(nums)
+nom = list(num)
+print(nom)
+print(type(nums))
+print(type(num))
