b6e395c276dcf7c386fedbf31b63a1019b94ff73
Dilber Husain Khan <53649013+DilberHusainKhan@users.noreply.github.com>
2021-03-21 15:18:20 +0530

    compare list

---
+++ chapter5/compare_list.py
@@ -0,0 +1,6 @@
+friuts = ['orange','apple','pear']
+friuts2 = ['banana','kiwi','apple','banana']
+friuts3 = ['orange','apple','pear']
+print(friuts==friuts3)  #Compare the value 
+print(friuts is friuts3)  #Compare the memory location
+print(friuts==friuts2)
