8d9c86e8579a91090506579d4b14544c2c604a41
Dilber Husain Khan <dilberjmi@gmail.com>
2021-03-22 11:00:44 +0530

    intro

---
+++ chapter7/intro_dictionary.py
@@ -0,0 +1,10 @@
+# because of limitation of lists, list are not enough to represent real data.
+x= 'abcdef'
+i='a'
+while i in x:
+    x = x[:-1]
+    print(i, end= " ")
+user = dict(name = "Dilber", age = "23")
+print(user)
+
+
