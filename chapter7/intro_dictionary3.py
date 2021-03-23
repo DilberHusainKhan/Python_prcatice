35b2d4f9d0880709e1c607e4a252457a88488475
Dilber Husain Khan <dilberjmi@gmail.com>
2021-03-23 20:03:29 +0530

    dictionary several methods

--- chapter7/intro_dictionary3.py
+++ chapter7/intro_dictionary3.py
@@ -23,11 +23,43 @@ else:
     print("Value is not present")
 #3.3 To check complete list is present in dictionary or not.
 if ['Dilber','Husain','Khan'] in user_info.values():
-    print(user_info.values())
-    print(user_info.items())
+    print(user_info)
+    # print(user_info.items())
 else:
     print('Not Present')
 
-#3.4 Loop in dictionary
+###3.4 Loop in dictionary
+# ***TO Print Keys in the dictionary
 for i in user_info:
     print(i)
+
+print("***To print values of the Dictionary")
+for i in user_info.values():
+    print(i)
+    #OR 
+for i in user_info:
+    print(user_info[i]) 
+
+#3.5 VALUES METHOD {values()}
+val_values = user_info.values()
+print(val_values)
+print(type(val_values)) #type of this val is dictionary
+
+
+#3.6 KEYS METHOD {keys()}
+val_key = user_info.keys()
+print(val_key)
+print(type(val_key))
+
+# 3.7 ITEMS METHOD {items()}
+
+user_item = user_info.items()
+print(user_item)
+print(type(user_item))
+
+# Why item is important  
+for key , value in user_info.items():
+     print(f'Key is {key} and value is {value}')
+
+for i,j in user_info.items():
+    print(f'Key of user is {i} and value is {j}')
