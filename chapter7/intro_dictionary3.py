f8df9fe39c121423f191ec48d24b54dddde52eb8
Dilber Husain Khan <dilberjmi@gmail.com>
2021-03-23 17:59:06 +0530

    dictionary more info

--- chapter7/intro_dictionary3.py
+++ chapter7/intro_dictionary3.py
@@ -1,1 +1,24 @@
-# Sample page
+# user_info dictionary 
+user_info ={
+    'rollnumber':1901320,
+    'name': ['Dilber','Husain','Khan'],
+    'age':24,
+    'education':{
+        '10':'CPVN',
+        '12': 'CPVN',
+        'Diploma': 'Jamia Millia Islamia'
+    }
+}
+
+# to check key present in dictionary
+if 'name' in user_info:
+    print(user_info['name'])
+else:
+    print('key is not present')
+
+# To check if value exist in dictionary
+if 24 in user_info.values():
+    print(user_info.keys())
+else:
+    print("Value is not present")
+
