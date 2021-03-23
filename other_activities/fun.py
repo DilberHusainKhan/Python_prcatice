c2ed021459d5ab1f13d68e453f81cfa18aa33957
Dilber Husain Khan <dilberjmi@gmail.com>
2021-03-23 17:21:51 +0530

    extra activities with python

---
+++ other_activities/fun.py
@@ -0,0 +1,28 @@
+# Swap two number 
+x,y =10,20
+print(x,y)
+
+x,y = y,x
+print(x,y)
+
+# create a single string from all the elements in the list
+a= ["Dilber", "Husain","Khan"]
+print(" ".join(a))
+
+# print the file Path of imported modules
+import os
+import socket
+print(os)
+print(socket)
+
+# return multiple Value from a function
+def x():
+    return 1,2,3,4
+a,b,c,d = x()
+print(a,b,c,d)
+
+# check the memory Usage of an object
+import sys
+x = 1
+print(sys.getsizeof(x)) 
+
