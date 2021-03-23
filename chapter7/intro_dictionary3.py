dc3b1567976ce47d66687111bb15969284dedf02
Dilber Husain Khan <dilberjmi@gmail.com>
2021-03-23 19:02:09 +0530

    update dictionary 3

--- chapter7/intro_dictionary3.py
+++ chapter7/intro_dictionary3.py
@@ -10,15 +10,24 @@ user_info ={
     }
 }
 
-# to check key present in dictionary
+# 3.1 to check key present in dictionary
 if 'name' in user_info:
     print(user_info['name'])
 else:
     print('key is not present')
 
-# To check if value exist in dictionary
+#3.2 To check if value exist in dictionary
 if 24 in user_info.values():
     print(user_info.keys())
 else:
     print("Value is not present")
+#3.3 To check complete list is present in dictionary or not.
+if ['Dilber','Husain','Khan'] in user_info.values():
+    print(user_info.values())
+    print(user_info.items())
+else:
+    print('Not Present')
 
+#3.4 Loop in dictionary
+for i in user_info:
+    print(i)
