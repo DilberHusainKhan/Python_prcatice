f37539e7a52f9fec31a7c1c26e553688026b1c85
Dilber Husain Khan <dilberjmi@gmail.com>
2021-03-24 12:49:50 +0530

    pop method , popitem methon

---
+++ chapter7/add_and_delete_dict.py
@@ -0,0 +1,35 @@
+user_info ={
+    'rollnumber':1901320,
+    'name': ['Dilber','Husain','Khan'],
+    'age':24,
+    'education':{
+        '10':'CPVN',
+        '12': 'CPVN',
+        'Diploma': 'Jamia Millia Islamia'
+    }
+}
+
+#To Add key value in dictionary
+user_info['Technical_skill'] = ['C','C++','Python','Java','Flutter']
+
+# POP METHOD
+# pop method will delete the item on the bases of key 
+# and return the value of deleted item & you must pass altest one variable in pop method
+
+# poped_item = user_info.pop('education')
+# print(f"Poped item is {poped_item}")
+
+
+# popitem method
+# it delete any ramdom value in the dictionary
+
+pop_by_item_method = user_info.popitem()
+
+print(f"Poped item by item method {pop_by_item_method}")
+print(type(pop_by_item_method))
+
+
+
+
+
+print(user_info)
