27755e27db942b8152e2dfbbd96ae97a6f599621
Dilber Husain Khan <dilberjmi@gmail.com>
2021-03-24 14:37:37 +0530

    update method

---
+++ chapter7/update_method.py
@@ -0,0 +1,23 @@
+# make dictionary
+user_info ={
+    'rollnumber':1901320,
+    'name': ['Dilber','Husain','Khan'],
+    'age':24,
+    'education':{
+        '10':'CPVN',
+        '12': 'CPVN',
+        'Diploma': 'Jamia Millia Islamia'
+    }
+}
+print(f"before update {user_info} .")
+
+more_info = {
+    'name' : ['Humayun', 'Anwar', 'Khan'],
+    'state' : 'Uttar Pradesh',
+    'hobbies' : ['Coding', 'Listening Music', 'Cooking']
+}
+
+# update() method
+user_info.update(more_info)
+
+print(f"After update more_info, name will also update {user_info} .")
