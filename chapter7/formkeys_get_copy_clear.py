3142f7b2ff204b238e97d60689ef97dd1615bc86
Dilber Husain Khan <dilberjmi@gmail.com>
2021-04-06 23:35:42 +0530

    get method

--- chapter7/formkeys_get_copy_clear.py
+++ chapter7/formkeys_get_copy_clear.py
@@ -11,3 +11,25 @@ print(c)
 print(d)
 e = dict.fromkeys(['name','age','height'], ['DILBER', 'HUSAIN'])
 print(e)
+
+# GET menthod (useful)
+name = {
+    'first': 'Humayun',
+    'middle': 'Anwar',
+    'last': 'Khan'
+}
+
+print(name['first'])
+# print(name['middle name'] #print error if key is not found
+print(name.get('first name')) # print none if key is not found
+
+if 'first' in name:
+    print('key is Present')
+else:
+    print('Key ks not present')
+
+# OR
+if name.get('first name'):
+    print("Present")
+else:
+    print("key is not present") 
