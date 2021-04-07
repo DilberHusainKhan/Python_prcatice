3003013cec599e8a0aabc787158024e26df29f83
Dilber Husain Khan <dilberjmi@gmail.com>
2021-04-07 23:35:47 +0530

    override of key values in dictionary

--- chapter7/more_about_get.py
+++ chapter7/more_about_get.py
@@ -5,3 +5,11 @@ user = {
 # to print  NOT FOUND instead of none
 print(user.get('names','NOT FOUND'))
 print(user.get('name','NOT FOUND'))
+
+# if we have more then one same keys in the dictionary then last value will be overide other
+user1 ={
+    'name': 'Dilber Husain Khan',
+    'age': 23,
+    'age':32,
+}
+print(user1)
