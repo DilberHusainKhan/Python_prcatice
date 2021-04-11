0d35563a936f5e39290d24a15570fef95a2e3e2f
Dilber Husain Khan <dilberjmi@gmail.com>
2021-04-11 23:03:56 +0530

    wrod counter in dictionary

---
+++ chapter7/EX_2_wordCounter.py
@@ -0,0 +1,10 @@
+# Word Counter
+# dilber
+# {'d': 1, 'i': 1 ...}
+s = (input("Enter String ").lower()).strip( )
+def Word_counter(s):
+    counter ={}
+    for i in s:
+        counter[i] = s.count(i)
+    return counter
+print(Word_counter(s))
