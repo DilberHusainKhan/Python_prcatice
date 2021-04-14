571189b72d754898f19485391a54f5cf28d7b2b1
Dilber Husain Khan <dilberjmi@gmail.com>
2021-04-14 23:41:21 +0530

    Ex 3

---
+++ store_data_form_user.py
@@ -0,0 +1,17 @@
+# program to store data form user in dictionary
+
+
+user = {}
+name = input("Enter you name \n")
+age = int(input("Enter your age \n"))
+fav_song = input("Enter fav songs seprated by , \n").split(',')
+fav_movie = input("Enter fav movies seprated by , \n").split(',')
+  
+user['name'] = name
+user['age'] = age
+user['fav_song'] = fav_song
+user['fav_movie'] = fav_movie
+
+print(user)
+
+
