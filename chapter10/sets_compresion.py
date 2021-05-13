a1199fca34901c2e99d190a217273a276789ff83
Dilber Husain Khan <dilberjmi@gmail.com>
2021-05-13 22:23:19 +0530

    sets compresion

---
+++ chapter10/sets_compresion.py
@@ -0,0 +1,8 @@
+# Set Compresion in python 
+# to print square of number
+sq = {k**2 for k in range(1,11)}
+print(sq)
+# to print first letter of names in the list 
+names = ['Dilber', 'Husain','Khan','Humayun','Anwar']
+first = {name[0] for name in names}
+print(first)
