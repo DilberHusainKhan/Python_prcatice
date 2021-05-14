234dd528c29ee337e6a2b1ca46431fda2f922e3e
Dilber Husain Khan <dilberjmi@gmail.com>
2021-05-14 15:13:23 +0530

    *args in python

---
+++ chapter11/intro_to_Star_args.py
@@ -0,0 +1,19 @@
+# make flexible function
+# *Operator
+# *args
+# Let we have a function 
+def total(a,b):
+    return a+b
+# print(total(3,4,10,9))  #TypeError: total() takes 2 positional arguments but 4 were given
+
+#   To solve this error we will use *args
+def all_total(*args):
+    # 1,2,3,4,5,6,7,89,9
+    total = 0
+    for num in args:
+        total +=num
+    return total
+print(all_total(1,2,3,4,5,6,7,89,9))
+
+
+
