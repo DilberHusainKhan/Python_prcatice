dfa195d9570ec913886fb6ebe01af4de6c8e8e61
Dilber Husain Khan <dilberjmi@gmail.com>
2021-05-22 13:52:11 +0530

    GUI application to open website on browser

---
+++ other_activities/Open_Google.py
@@ -0,0 +1,24 @@
+# import webbrowser module
+import webbrowser
+# import tkinker
+from tkinter import *
+# Creating root
+root = Tk()
+# setting GUI Tittle
+root.title("Web Browser")
+# setting GUI size
+root.geometry("300x200")
+# function to open webtyx
+def webtyx():
+    webbrowser.open("www.webtyx.com")
+# function to open google
+def google():
+    webbrowser.open("www.google.com")
+# button to call webtyx
+webtyx = Button(root, text= "Visit webtyx", command= webtyx).pack(pady=20)
+# button to call google
+google = Button(root, text= "Open Google", command= google).pack(pady=20)
+
+root.mainloop()
+
+
