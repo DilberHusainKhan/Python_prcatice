9a4118d0247e157b30617d607048b7f1720f6439
dilberhusainkhan <dilberjmi@gmail.com>
2022-07-10 09:44:38 +0530

    if else condition

---
+++ other_activities/If_Else.py
@@ -0,0 +1,7 @@
+name = input("Enter your name ")
+age = int(input('Enter Your age '))
+
+if age>= 18:
+    print(name,' sir, You are 18+ allow')
+else:
+    print(name,' Sir you are not 18+ not allow')
